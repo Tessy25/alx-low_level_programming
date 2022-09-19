6dae838b3136ff8e7dccd2a2ebfe3a1681dba8fa
Tessy25 <ajitessy25@gmail.com>
2022-09-19 12:33:09 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts _ prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
