efe90af70d214e32f7606dfbf0e22bf1ef8155d5
Tessy25 <ajitessy25@gmail.com>
2022-09-19 19:47:47 -0700

    array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints an inputted number of elements
+ * of an array of intergers.
+ * @a: the array of integers
+ * @n: the number of elementsto be printed.
+ */
+void print_array(int *a, int n)
+{
+	int index;
+
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", a[index]);
+
+		if (index == n - 1)
+			continue;
+		printf(",");
+	}
+	printf("\n");
+}
